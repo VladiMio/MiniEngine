5a9afae2e7711ea22bbb8be5ca77cf4a91cc8421
VladiMio <jasper1992ws@hotmail.com>
2018-03-05 19:47:23 +0800

    updated

--- enc_temp_folder/bca1dbea6f7c5eba5d543f4ea0f6e565/Source.cpp
+++ enc_temp_folder/bca1dbea6f7c5eba5d543f4ea0f6e565/Source.cpp
@@ -60,7 +60,20 @@ int main()
 
 	glViewport(0, 0, 800, 600);
 	
-#pragma region Complie Shaders
+	unsigned int VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 
 	int  success;
 	char infoLog[512];
@@ -99,24 +112,6 @@ int main()
 	}
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-
-#pragma endregion
-
-
-	//Create VAO and VBO
-	unsigned int VAO, VBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
